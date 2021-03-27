fed5d5e210a26d376e9f3e4dd9755537c28e92a1
hojun.lee <60565211+paullabkorea@users.noreply.github.com>
2021-03-27 21:48:07 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://ridibooks.com/category/new-releases/2200'
+url = 'https://ridibooks.com/category/bestsellers/2200'
 response = requests.get(url)
 response.encoding = 'utf-8'
 html = response.text
