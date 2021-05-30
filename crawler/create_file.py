d82da4ea372076f1f170a7ea2d97b693f426979e
Taeuk Kang <mail@taeuk.me>
2021-05-30 22:24:41 +0900

    Coverage report 추가

--- crawler/create_file.py
+++ crawler/create_file.py
@@ -2,4 +2,4 @@ import json
 
 def write_json(json_data, filepath):
     with open(filepath, 'w', encoding='UTF-8-sig') as f_write:
-        json.dump(json, f_write, ensure_ascii=False, indent=4)
+        json.dump(json_data, f_write, ensure_ascii=False, indent=4)
